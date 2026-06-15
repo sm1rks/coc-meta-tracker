0e9f7279d7897fa4b928f5f87d9d2d3a336bf051
sm1rks <jo873558@ucf.edu>
2026-06-15 00:16:00 -0400

    clean up project workspace and update .gitignore

--- scratch/make_test6.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('scripts/fetch-meta.ts', 'utf8');
-const script = code.replace(
-  'const armyName =',
-  'if (coreTroop === "Thrower") { console.log(JSON.stringify(troopCounts, null, 2)); } const armyName ='
-);
-fs.writeFileSync('scratch/test6.ts', script);
--- scratch/test5.js
+++
@@ -1,6 +0,0 @@
-const data = require('./data/meta.json');
-const throwerArmy = data.armies.find(a => a.name.includes('Thrower'));
-console.log('Thrower army in meta.json:', JSON.stringify(throwerArmy, null, 2));
-
-// find actual players that use Thrower
-// We don't have raw share codes in meta.json.
